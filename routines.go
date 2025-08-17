a039b766b9eac2c2cbe18d90350771e2a9099692
ayushhhhyqxv <ayushpadhaikar@gmail.com>
2025-08-17 11:27:30 +0530

    Implementation of go routines

---
+++ routines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func task(id int , wg *sync.WaitGroup){
+	defer wg.Done()
+	fmt.Println("Doing Task !",id)
+
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i:=0;i<10;i++{
+		wg.Add(1)
+		go task(i,&wg)
+		// go func (i int ){
+		// 	fmt.Println(i)
+		// }(i)
+	}
+	wg.Wait()
+}
